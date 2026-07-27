0fb3f8a0ad04d98c041ca3d0b1dcb9a5b716a3dd
mdot <mohammadmunti.5@gmail.com>
2026-07-27 21:16:36 +0530

    demo of msgparser

--- src/parser/reader.js
+++ src/parser/reader.js
@@ -1,7 +1,11 @@
+import { log } from 'console';
 import { createReadStream } from 'fs';
+import { type } from 'os';
 import readline from 'readline/promises'; // Import as lowercase 'readline'
 
-const fileStream = createReadStream('/home/mdot/projectFolder/whatsapp-export-parser/sample.txt', { encoding: 'utf8' });
+const fileStream = createReadStream('/Users/mohammadmuntazir/gitrepo/whatsapp-export-parser/sample.txt', { encoding: 'utf8' });
+const demoText = "20/07/2026, 10:15 AM - Ali: Hey, how are you?";
+let trackCurrectMsg = "";
 
 // Use lowercase 'readline' here
 const rl = readline.createInterface({
@@ -9,6 +13,17 @@ const rl = readline.createInterface({
   crlfDelay: Infinity
 });
 
-for await (const line of rl) {
-  console.log(line);
+// implementing parser - demo
+
+function msgParser(demoText) {
+  let dateString = demoText.slice(0, 10);
+  let [day, month, year] = dateString.split("/");
+  let dateObj = new Date(year, month - 1, day);
+  console.log(dateObj.toLocaleDateString('en-GB'));
 }
+
+msgParser(demoText)
+
+// for await (const line of rl) {
+//   console.log(line);
+// }
