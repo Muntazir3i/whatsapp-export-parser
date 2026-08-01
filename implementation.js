c19f77f21909fc2103be61d1124f461160db00bf
MDOT <mohammadmunti.5@gmail.com>
2026-08-01 12:22:45 +0530

    Date Check Not working Properly

--- implementation.js
+++ implementation.js
@@ -25,41 +25,62 @@
 // console.log(result)
 //
 
-export function isMessageStartLine(lineText) {
-  let dateString = lineText.slice(0, 8);
-  if (dateString.length === 6) {
-    let newDateString = dateString.replace(
-      /^(\d{1,2})\/(\d{1,2})\/(\d{2})$/,
-      "$2/$1/$3",
-    );
-    let [day, month, year] = newDateString.split("/");
-    let dateObj = new Date(year, month - 1, day);
-    console.log(dateObj);
-  }
+// export function isMessageStartLine(lineText) {
+//   let dateString = lineText.slice(0, 8);
+//   // if (dateString.length === 6) {
+//   //   let newDateString = dateString.replace(
+//   //     /^(\d{1,2})\/(\d{1,2})\/(\d{2})$/,
+//   //     "$2/$1/$3",
+//   //   );
+//   //   let [day, month, year] = newDateString.split("/");
+//   //   let dateObj = new Date(year, month - 1, day);
+//   //   console.log(dateObj);
+//   // }
 
-  if (dateString.length === 7) {
-    let newDateString = dateString.replace(
-      /^(\d{1,2})\/(\d{1,2})\/(\d{2})$/,
-      "$2/$1/$3",
-    );
-    let [day, month, year] = newDateString.split("/");
-    let dateObj = new Date(year, month - 1, day);
-    console.log(dateObj);
-  }
+//   // if (dateString.length === 7) {
+//   //   let newDateString = dateString.replace(
+//   //     /^(\d{1,2})\/(\d{1,2})\/(\d{2})$/,
+//   //     "$2/$1/$3",
+//   //   );
+//   //   let [day, month, year] = newDateString.split("/");
+//   //   let dateObj = new Date(year, month - 1, day);
+//   //   console.log(dateObj);
+//   // }
 
-  let [day, month, year] = dateString.split("/");
-  let dateObj = new Date(year, month - 1, day);
-  console.log(dateObj.toLocaleDateString());
+//   let [day, month, year] = dateString.split("/");
+//   // let fullYear = "20"+year;
+//   let dateObj = new Date(year, month - 1, day);
+//   console.log(!Number.isNaN(dateObj.getDate()));
+//   // return !Number.isNaN(dateObj.getDate());
+// }
 
-  // console.log(dateObj);
-  // return !Number.isNaN(dateObj.getDate());
+// isMessageStartLine("2/3/20");
+
+export function parseMessageText(rawMessageText) {
+    const [timestampStr, rest] = rawMessageText.split(" - ");
+    const [sender, ...msgParts] = rest.split(": ");
+    const messageContent = msgParts.join(": ");
+    const [dateStr, timeStr] = timestampStr.split(", ");
+
+    return {
+        sender: sender,
+        message: messageContent,
+        timestamp: `${dateStr} ${timeStr}`,
+    };
 }
 
-isMessageStartLine("12/05/25");
+// let obj = parseMessageText("10/5/20, 11:27 PM - Mir Aatif: <Media omitted>")
+// console.log(obj);
 
-/**
-console.log(dateString);
-console.log(dateString.length) ;
-  let [day, month, year] = dateString.split("/");
-let dateObj = new Date(year, month - 1, day);
- */
+export function isMessageStartLine(lineText) {
+    const [timestampStr, rest] = lineText.split(" - ");
+    console.log(timestampStr);
+    // let [day, month, year] = dateString.split("/");
+    // let fullYear = "20"+year;
+    // let dateObj = new Date(fullYear, month - 1, day);
+    // return !Number.isNaN(dateObj.getDate());
+}
+
+let result = isMessageStartLine("10/5/20, 11:27 PM - Mir Aatif: <Media omitted>")
+console.log(result);
+
